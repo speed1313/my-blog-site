3616f9bf1e8f6ced25e3fd47b7b22cd83ba3eea1
speed1313 <speedtry13@icloud.com>
2021-09-01 16:53:53 +0900

    fix: move  google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `G-MQB04MPT9N`,
+        trackingId: "process.env.GOOGLE_ANALYTICS_ID",
         head: true,
       },
     },
